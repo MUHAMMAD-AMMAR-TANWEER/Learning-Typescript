46af6f07e83bc0e794ae936e326a79916f6f9149
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-07-27 11:39:28 +0500

    corrected the comiplation error

--- todo/dist/index.js
+++ todo/dist/index.js
@@ -1,2 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Item_1 = require("./Item");
+let item = new Item_1.Item(11, 'first_task', false);
+item.printTask();
--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,1 +1,6 @@
 import { Item } from "./Item";
+
+
+let item : Item = new Item(11, 'first_task', false);
+
+item.printTask();
