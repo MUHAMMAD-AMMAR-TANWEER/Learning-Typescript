acfee3fe5c420ef1c0be637dd29e9a22f5780a7c
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-07-27 11:46:27 +0500

    updated the javascript using compiler running

--- todo/dist/index.js
+++ todo/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Item_1 = require("./Item");
-let item = new Item_1.Item(11, 'first_task', false);
+let item = new Item_1.Item(11, 'first_task', false); //let's build two more objects
+let item1 = new Item_1.Item(12, "second_task", false);
+let item2 = new Item_1.Item(13, "Task 3");
 item.printTask();
+item1.printTask();
+item2.printTask();
