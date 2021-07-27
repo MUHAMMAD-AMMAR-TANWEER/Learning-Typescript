4f616af9f212e9af7c11cc64950bb2f2a7795f6f
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-07-27 11:45:41 +0500

    created more objects for it

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,6 +1,13 @@
 import { Item } from "./Item";
 
 
-let item : Item = new Item(11, 'first_task', false);
+let item : Item = new Item(11, 'first_task', false);//let's build two more objects
+let item1 : Item =  new Item(12 ,  "second_task", false);
+let item2  : Item =  new Item(13, "Task 3");
+
+
+
 
 item.printTask();
+item1.printTask();
+item2.printTask();
